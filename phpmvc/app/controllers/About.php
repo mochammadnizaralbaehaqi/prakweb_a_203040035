51f615f1a7a6a81796ff53724faf9a1907a94085
mochammadnizar <mochammadnizaralbaehaqi@example.com>
2022-10-10 15:34:51 +0700

    Menyelesaikan video 5

--- phpmvc/app/controllers/About.php
+++ phpmvc/app/controllers/About.php
@@ -1,12 +1,20 @@
 <?php
 
-class About {
-    public function index($nama = 'Mochammad Nizar Albaehaqi', $pekerjaan = 'Mahasiswa')
+class About extends Controller {
+    public function index($nama = 'Mochammad Nizar Albaehaqi', $pekerjaan = 'Mahasiswa', $umur = 20)
     {
-        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $this->view('templates/header');
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
+    
     public function page()
     {
-        echo 'About/page';
+        $this->view('templates/header');
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
