535669366a0d8abbba1cab043a9b0fd0bd98428e
mochammadnizar <mochammadnizaralbaehaqi@example.com>
2022-10-10 22:50:23 +0700

    Menyelesaikan video 7

--- phpmvc/app/controllers/Home.php
+++ phpmvc/app/controllers/Home.php
@@ -4,6 +4,7 @@ class Home extends Controller {
     public function index()
     {
         $data['judul'] = 'Home';
+        $data['nama'] = $this->model('user_model')->getuser();
         $this->view('templates/header', $data);
         $this->view('home/index');
         $this->view('templates/footer');
---
+++ phpmvc/app/models/Mahasiswa_model.php
@@ -0,0 +1,12 @@
+<?php
+
+class Mahasiswa_model {
+    private $mhs = [
+        {
+            "nama" => "Mochammad Nizar Albaehaqi",
+            "nrp" => "203040035",
+            "email" => "mochammadnizar10@gmail.com",
+            "jurusan" => "Teknik Informatika"
+        }
+    ]
+}
